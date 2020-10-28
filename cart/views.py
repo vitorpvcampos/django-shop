87d58c5475fa028d4059af24d5c1b65ee37986dc
uraeus <vitor.campos@engenharia.ufjf.br>
2020-10-28 16:32:19 -0300

    Template to display the cart close #15

--- cart/views.py
+++ cart/views.py
@@ -12,8 +12,10 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
-    return redirect('cart:card_detail')
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
 
 
 @require_POST
