75c4ec511e40c8ea835d8169ab454017e5d72c54
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-11-13 12:11:53 -0300

    Administration site extended with custom views close #33

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 
 def order_create(request):
@@ -22,3 +24,9 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
